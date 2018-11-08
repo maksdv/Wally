1a81a65fc7ab0d168961949472e04c7d069c4781
maks <maxdv31@gmail.com>
2018-11-08 13:33:15 +0100

    Navigation with icons

--- client/src/navigation.js
+++ client/src/navigation.js
@@ -1,20 +1,21 @@
-import React, { Component, Image } from 'react';
+import React,{ Component } from 'react';
 
 import {
   StackActions,
   NavigationActions,
   createStackNavigator,
-  createMaterialTopTabNavigator,
+  createBottomTabNavigator,
 } from 'react-navigation';
 import {
   reduxifyNavigator,
   createReactNavigationReduxMiddleware,
 } from 'react-navigation-redux-helpers';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet,BackHandler } from 'react-native';
 import { connect } from 'react-redux';
 import Articles from './screens/articles.screen';
 import InfoArticles from './screens/infoArticle.screen';
 import Chats from './screens/chats.screen';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 
 const styles = StyleSheet.create({
@@ -25,46 +26,56 @@ const styles = StyleSheet.create({
     backgroundColor: 'red',
   },
 });
-
-const Prueba = () => <Image source="./media/tapusguapus.jpg" style={{ width: 30, height: 30 }} />;
-
-class SettingsScreen extends Component {
-  static navigationOptions = {
-    tabBarLabel: 'mecagoendios',
-    tabBarIcon: <Image source="./media/tapusguapus.jpg" style={{ width: 30, height: 30 }} />,
-  };
-
-  render = () => <View />;
-}
-
 const TestScreen = title => () => (
   <View style={styles.container}>
     <Text>{title}</Text>
   </View>
 );
 // tabs in main screen
-const MainScreenNavigator = createMaterialTopTabNavigator(
+const MainScreenNavigator = createBottomTabNavigator(
   {
-    Rayitas: {
-      screen: SettingsScreen,
-      navigationOptions: {
-        tabBarLabel: 'mecagoendios',
-        tabBarIcon: <Image source="./media/tapusguapus.jpg" style={{ width: 30, height: 30 }} />,
-        tabBarColor: '#ffff',
-      },
-    },
-    Buscador: { screen: Articles },
-    Chats: { screen: Chats },
-    Settings: { screen: InfoArticles },
+    Buscador: {screen: Articles,
+    navigationOptions: {
+      tabBarLabel: 'Search',
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="ios-search" color={tintColor} size={24} />
+      )
+    }
   },
+    Settings: { screen: TestScreen('Settingea algo pisha'),
+    navigationOptions: {
+      tabBarLabel: 'Settings',
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="ios-settings" color={tintColor} size={24} />
+      )
+    }
+  },
+  Messeges: { screen: Chats,
+  navigationOptions: {
+    tabBarLabel: 'Messeges',
+    tabBarIcon: ({tintColor}) => (
+      <Icon name="ios-mail" color={tintColor} size={24} />
+    )
+  }
+},
+  
+  }, // fuera de mainscreen
   {
     initialRouteName: 'Buscador',
-  },
+    order: ['Messeges','Buscador', 'Settings'],
+    navigationOptions: {
+        tabBarVisible: true
+      },
+      tabBarOptions: {
+        activeTintColor: 'red',
+        inactiveTintColor: '#4b6fe5'
+      }
+      }
 );
 const AppNavigator = createStackNavigator(
   {
     Main: { screen: MainScreenNavigator },
-    InfoArticles: { screen: InfoArticles },
+    InfoArticles: { screen: InfoArticles},
   },
   {
     headerMode: 'none',
@@ -81,11 +92,16 @@ const initialState = AppNavigator.router.getStateForAction(
     ],
   }),
 );
+
+
+
 export const navigationReducer = (state = initialState, action) => {
   const nextState = AppNavigator.router.getStateForAction(action, state);
   // Simply return the original `state` if `nextState` is null or undefined.
   return nextState || state;
 };
+
+
 // Note: createReactNavigationReduxMiddleware must be run before createReduxBoundAddListener
 export const navigationMiddleware = createReactNavigationReduxMiddleware(
   'root',
@@ -95,5 +111,25 @@ const App = reduxifyNavigator(AppNavigator, 'root');
 const mapStateToProps = state => ({
   state: state.nav,
 });
-const AppWithNavigationState = connect(mapStateToProps)(App);
+
+class AppWithBackPress extends Component {
+  componentDidMount() {
+    BackHandler.addEventListener('hardwareBackPress', this.onBackPress);
+  }
+
+  componentWillUnmount() {
+    BackHandler.removeEventListener('hardwareBackPress', this.onBackPress);
+  }
+
+  onBackPress = () => {
+    const { dispatch } = this.props;
+    dispatch(NavigationActions.back());
+    return true;
+  };
+
+  render() {
+    return <App {...this.props} />;
+  }
+}
+const AppWithNavigationState = connect(mapStateToProps)(AppWithBackPress);
 export default AppWithNavigationState;
