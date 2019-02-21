47b92019f695a0dc41400840f80ad1ff6aaac01c
maks <maxdv31@gmail.com>
2019-02-21 13:39:03 +0100

    Commit 21/02

--- client/src/app.js
+++ client/src/app.js
@@ -1,27 +1,23 @@
 
 import React from 'react';
 import thunk from 'redux-thunk';
+
 import { setContext } from 'apollo-link-context';
 import { ApolloClient } from 'apollo-client';
 import { ApolloLink } from 'apollo-link';
 import { ApolloProvider } from 'react-apollo';
-import { WebSocketLink } from 'apollo-link-ws';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { createHttpLink } from 'apollo-link-http';
-import { getMainDefinition } from 'apollo-utilities';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { ReduxCache, apolloReducer } from 'apollo-cache-redux';
 import ReduxLink from 'apollo-link-redux';
-import Config from 'react-native-config';
-import { SubscriptionClient } from 'subscriptions-transport-ws';
 import { onError } from 'apollo-link-error';
 import AppWithNavigationState, { navigationReducer, navigationMiddleware } from './navigation';
 import auth from './reducers/auth.reducer';
 
+const URL = 'ec2-18-221-123-227.us-east-2.compute.amazonaws.com:8080';
 
-//const URL = 'ec2-18-221-123-227.us-east-2.compute.amazonaws.com:8080';
-const URL = '192.168.1.132:8080';
 const store = createStore(
   combineReducers({
     apollo: apolloReducer,
@@ -51,34 +47,8 @@ const middlewareLink = setContext((req, previousContext) => {
   return previousContext;
 });
 
-export const wsClient = new SubscriptionClient(
-  `ws://${Config.SERVER || 'localhost'}:${Config.PORT || '80'}/graphql`,
-  {
-    reconnect: true,
-    connectionParams: {
-      // Pass any arguments you want for initialization
-    },
-  },
-);
 
-const webSocketLink = new WebSocketLink(wsClient);
-
-const requestLink = ({ queryOrMutationLink, subscriptionLink }) => ApolloLink.split(
-  ({ query }) => {
-    const { kind, operation } = getMainDefinition(query);
-    return kind === 'OperationDefinition' && operation === 'subscription';
-  },
-  subscriptionLink,
-  queryOrMutationLink,
-);
-const link = ApolloLink.from([
-  reduxLink,
-  errorLink,
-  requestLink({
-    queryOrMutationLink: middlewareLink.concat(httpLink),
-    subscriptionLink: webSocketLink,
-  }),
-]);
+const link = ApolloLink.from([reduxLink, errorLink, httpLink]);
 
 export const client = new ApolloClient({
   link,
